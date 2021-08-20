d9b49c79c4e67cb3d7225de120670dd57edb7c68
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-19 17:56:27 -0700

    saveRecord

--- public/offline.js
+++ public/offline.js
@@ -1,14 +1,28 @@
+let db;
+
 const request = window.indexedDB.open("budget", 1);
 
 request.onupgradeneeded= ( event ) => {
   const db = event.target.result;
-  db.createObjectStore("pending", { autoIncremenet: true })
+  db.createObjectStore("pending", { autoIncrement: true })
 };
 
 request.onsuccess = event => {
-  const db = request.result;
+  db = request.result;
 
   if (navigator.onLine) {
     checkDatabase();
   }
 };
+
+request.onerror = function (event) {
+  console.log('unable to connect to indexDB budget database' + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['pending'], 'readwrite');
+
+  const store = transaction.createObjectStore('pending');
+
+  store.onupgradeneeded(record);
+}
